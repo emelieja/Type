cadeac3744bd5009f0b89a6184409552b149805d
Emelie <Emiluen@MacBook-Air-2.lan>
2016-12-12 11:29:58 +0100

    Added basic routes

--- client/src/app.js
+++ client/src/app.js
@@ -1,7 +1,52 @@
-import angular from 'angular';
-angular.module('type', [])
-.controller('typeController', function($http) {
-    $http.get('/types').then((response) => {
-        this.types = response.data;
-    });
-});
+import angular from 'angular'
+import 'angular-ui-router'
+angular.module('type', ["ui.router"])
+
+.config(($stateProvider, $urlRouterProvider) => {
+    $urlRouterProvider.otherwise('/cards')
+
+    $stateProvider
+        .state('cards', {
+            url: '/cards',
+            templateUrl: 'cards/cards-nav.html',
+            resolve: {
+                cardsService: function ($http) {
+                    return $http.get('/types');
+                }
+            },
+            controller: function (cardsService) {
+                this.types = cardsService.data;
+            },
+            controllerAs: 'typeCtrl'
+        })
+        .state('cards.typology', {
+            url: '/:typologyName',
+            templateUrl: 'cards/cards-typology.html',
+            resolve: {
+                cardService: function ($q) {
+                    return $q((resolve, reject) => {
+                        let card = {
+                            "name": "Myers-Briggs",
+                            "goldMedals": [
+                                {
+                                    "division": "Top",
+                                    "country": "US",
+                                    "year": 2012
+                                },
+                                {
+                                    "division": "Good",
+                                    "country": "Sweden",
+                                    "year": 2013
+                                }
+                            ]
+                        };
+                        resolve({data: card});
+                    })
+                }
+            },
+            controller: function(cardService) {
+                this.card = cardService.data;
+            },
+            controllerAs: 'cardCtrl'
+        })
+})
